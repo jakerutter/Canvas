51053e0454264ae573cf126acadeb60dae08f8fd
Jacob Rutter <rutter.jacob@gmail.com>
2017-10-10 15:38:27 -0400

    Added ambient sounds
    Testing adding sounds to my canvas app

--- action.js
+++ action.js
@@ -1,7 +1,7 @@
 
 
 function draw() {
-    
+    sound();
     DrawSquares();
     RotateSquares();
   }
@@ -11,7 +11,7 @@ function draw() {
 function DrawSquares(){
     var ctx = document.getElementById('myCanvas').getContext('2d');
     var counter = 0;
-    var id = setInterval(frame, 100);
+    var id = setInterval(frame, 50);
     var x = 0;
     var y = 0;
     function frame() {
@@ -58,32 +58,9 @@ ctx.fillStyle = '#4D4E53';
 ctx.fillRect(150, 30, 100, 100);
 }
 
-
-
-// //This works if all esle fails go back to this
-// function DrawSquares(){
-//     var ctx = document.getElementById('myCanvas').getContext('2d');
-//     var counter = 0;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-
-//         if (counter == 144){
-//             clearInterval(id);
-//         } else {
-//             counter ++;
-//             for (var i = counter-1; i < 12; i++) {
-//                 for (var j = counter-1; j < 12; j++) {
-//                   ctx.save();
-//                   ctx.fillStyle = 'rgb(' + (21 * i) + ', ' + (255 - 21 * i) + ', 255)';
-//                   ctx.translate(10 + j * 50, 10 + i * 50);
-//                   ctx.fillRect(0, 0, 25, 25);
-//                   ctx.restore();
-//                     }
-//                 }
-//         }
-//   }
-// }
-
+function sound() {
+    document.getElementById('birdies').play();
+}
 
 
 // //draws a cool loading bar
